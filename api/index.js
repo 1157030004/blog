4585ac71e461b999af183503d33b926e74ac5070
1157030004 <1157030004@student.uinsgd.ac.id>
2021-09-10 15:33:44 +0700

    api port

--- api/index.js
+++ api/index.js
@@ -45,7 +45,7 @@ app.use("/api/users", usersRoute);
 app.use("/api/posts", postsRoute);
 app.use("/api/categories", catRoute);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
 	console.log(`Server is running on PORT ${PORT}`);
